46a35e784aa93dc09276d5807e011ef5620abec7
answer.zou <18500397765@163.com>
2016-07-19 17:15:56 +0800

    Initial Commit

---
+++ Swift-MVVMDemoTests/Swift_MVVMDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_MVVMDemoTests.swift
+//  Swift-MVVMDemoTests
+//
+//  Created by Iverson allen on 16/7/19.
+//  Copyright © 2016年 Iverson allen. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_MVVMDemo
+
+class Swift_MVVMDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
